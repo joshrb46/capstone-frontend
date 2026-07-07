be809f93b44e506351fd5a998d6ebb7ee4dea6f9
joshrb46 <joshrb46@gmail.com>
2026-07-07 09:42:53 -0700

    Fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 import Landing from "./pages/Landing";
+import Lobby from "./pages/Lobby";
 import Game from "./pages/Game";
+import Podium from "./pages/Podium";
 
 export default function App() {
   return (
@@ -9,7 +11,9 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/game/:code" element={<Game />} />
+          <Route path="/lobby/:code" element={<Lobby />} />
+          <Route path="/game/:code/:matchId" element={<Game />} />
+          <Route path="/podium/:code/:matchId" element={<Podium />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
